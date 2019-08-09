9345ada9b31c9dbf769f414ef78f79a7245a631a
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-08-10 02:34:51 +0300

    added UI

---
+++ Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+	[SerializeField] private Transform bar;
+	
+	// Use this for initialization
+	void Start()
+	{
+		SetSize(0.7f);
+	}
+	
+	public void SetSize(float sizeNormalized)
+	{
+		bar.localScale = new Vector3(sizeNormalized, 1f);
+	}
+	
+}
