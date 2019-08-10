64a1457ab8279777c91cee27a55f7fd3c585f103
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-08-10 12:15:10 +0300

    added firing butons

---
+++ Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/ShootingBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBehaviour : MonoBehaviour
+{
+	[SerializeField] private GameObject fireball;
+	[SerializeField] private GameObject iceball;
+
+	private float speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
