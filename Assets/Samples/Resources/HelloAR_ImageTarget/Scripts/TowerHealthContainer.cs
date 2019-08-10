cf200a8ec455196a2e46e9c9362bc1ee872c859a
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-08-10 17:02:47 +0300

    finished tower obtain

--- Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/TowerHealthContainer.cs
+++ Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/TowerHealthContainer.cs
@@ -7,16 +7,22 @@ public class TowerHealthContainer : MonoBehaviour
 	[SerializeField] private int health;
 
 	[SerializeField] private GameObject HealthBar;
-
+	[SerializeField] private ParticleSystem effectObtain;
 	[SerializeField] private GameObject alternateTower;
 	// Use this for initialization
 	// Update is called once per frame
+	void DisableEffect()
+	{
+		effectObtain.gameObject.SetActive(false);
+	}
 	void Update ()
 	{
 		if (health <= 0)
 		{
+			effectObtain.gameObject.SetActive(true);
 			gameObject.SetActive(false);
 			alternateTower.SetActive(true);
+			Invoke("DisableEffect", 1);
 			health = 100;
 		}
 		HealthBar.GetComponent<HealthBar>().SetSize(health / 100f);
