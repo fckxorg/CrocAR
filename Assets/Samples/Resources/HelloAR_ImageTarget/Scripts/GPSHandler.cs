ec531bbc830228301d525127f82bc432a26ebac2
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-08-10 20:22:32 +0300

    compass person detector

--- Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/GPSHandler.cs
+++ Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/GPSHandler.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class GPSHandler : MonoBehaviour
 {
     private Text text;
- 
+    private float angle = 250f;
+    [SerializeField] private GameObject healthBar;
     void Start()
     {
         text = gameObject.GetComponent<Text>();
@@ -19,11 +20,20 @@ public class GPSHandler : MonoBehaviour
     {
         if (Input.location.isEnabledByUser)
         {
+            
             float lat = Input.location.lastData.latitude;
             float lon = Input.location.lastData.longitude;
             float heading = Input.compass.magneticHeading;
+            if (Math.Abs(heading - angle) <= 10)
+            {
+                healthBar.SetActive(true);
+            }
+            else
+            {
+                healthBar.SetActive(false);
+            }
             text.text = "Depart lat: " + lat + " lon: " + lon +" magnetic: "+heading;
- 
+            
         }
         else
             text.text = "gps off";
