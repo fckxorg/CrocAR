bc6e78a66298a49ada63c7c47f35ab94fa42ccaa
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-08-10 19:43:52 +0300

    gps gathering

---
+++ Assets/Samples/Resources/HelloAR_ImageTarget/Scripts/GPSHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class GPSHandler : MonoBehaviour
+{
+    private Text text;
+ 
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        Input.location.Start();
+    }
+ 
+    private void Update()
+    {
+        if (Input.location.isEnabledByUser)
+        {
+            float lat = Input.location.lastData.latitude;
+            float lon = Input.location.lastData.longitude;
+           
+            text.text = "Depart lat: " + lat + "lon: " + lon;
+ 
+        }
+        else
+            text.text = "gps off";
+    }
+}
